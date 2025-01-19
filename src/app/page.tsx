268d830de2f65db6a39d64c28bb1bc9ec1344392
frixaco <rr.ashurmatov.21@gmail.com>
2025-01-19 23:19:33 +0500

    Join /pinecone with /

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { SearchButton } from "@/components/search-button";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { UploadTranscriptsButton } from "@/components/upload-transcripts-button";
 
 export default async function Home() {
   return (
     <div className="">
       <ThemeToggle />
       <SearchButton />
+
+      <UploadTranscriptsButton />
     </div>
   );
 }
