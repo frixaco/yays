b843acb98c009d030af2ce235480738829d15ce8
frixaco <rr.ashurmatov.21@gmail.com>
2025-01-19 23:04:15 +0500

    Add dark mode toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { SearchButton } from "@/components/search-button";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 export default async function Home() {
   return (
     <div className="">
+      <ThemeToggle />
       <SearchButton />
     </div>
   );
